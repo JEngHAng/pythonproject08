7769e5cdab29cf90a51e0fab254c7de0d6c46dce
JEngHAng <jangg500z@gmail.com>
2023-10-27 10:12:36 +0700

    test02

---
+++ test02.py
@@ -0,0 +1,16 @@
+# Dictionary คือ key : value
+my_dict1 = { "name":"mod" , "age":30 , 555:999 , "flag":True , "wow":[ 10 , 20 , 30 ]}
+my_dict2 = { "data1":[ 10 , 30 , 40 ] , 
+            "data2":( 2 , 5 , 6 ) , 
+            "data3":( 45 , 3 , 4 ) , 
+            "data4":{ "x":111 , 
+                     "y":222 }
+            }
+#การเข้าถึงข้อมูลใดข้อมูลหนึ่ง
+print (my_dict1["name"])
+print (my_dict1["age"])
+print (my_dict1[555])
+my_dict1["age"] = 50
+print (my_dict1["age"])
+
+#การเข้าถึงทุกข้อมูล
